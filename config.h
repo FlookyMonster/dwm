f23edeb814c274fc5a118098ea0a7ae0d1d36560
flookymonster <codenameflooent@gmail.com>
2023-09-25 22:48:25 -0300

    d

--- config.h
+++ config.h
@@ -13,15 +13,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:size=10" };
 static const char dmenufont[]       = "Terminus:size=10";
-static const char col_gray1[]       = "#2A2A2A";
-static const char col_gray2[]       = "#ffffff";
+static const char col_gray1[]       = "#5B544C";
+static const char col_gray2[]       = "#E4E3E0";
 static const char col_gray3[]       = "#ECECEC";
-static const char col_gray4[]       = "#70C0BA";
-static const char col_cyan[]        = "#000000";
+static const char col_gray4[]       = "#000000";
+static const char col_cyan[]        = "#DA2A47";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray2, col_gray1, col_cyan },
+	[SchemeSel]  = { col_gray2, col_cyan,  col_gray3  },
 };
 
 /* tagging */
@@ -61,6 +61,7 @@ static const char *mutecmd[] = { "volume_brightness", "volume_mute", NULL };
 static const char *volupcmd[] = { "volume_brightness", "volume_up", NULL };
 static const char *voldowncmd[] = { "volume_brightness", "volume_down", NULL };
 static const char *printscreen[] = { "screenshotthing", NULL };
+static const char *powermenu[] = { "shutdown-dmenu", NULL };
 
 
 /* key definitions */
@@ -120,7 +121,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,           {.v = powermenu} },
 };
 
 /* button definitions */
