2974d7efb0c7f8b2cc170cf576d7a3a3e42e2ca5
fluffeon <flookythecube@gmail.com>
2024-04-12 15:16:46 -0300

    e

--- run-this-to-install.sh
+++ run-this-to-install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-if [ "$EUID" != 0 ]; then	
+if [ "$EUID" != 0 ]; then
+
+	case $1 in
+		"help")
+			echo "./run-this-to-install.sh clean: Runs make clean install instead"
+			echo "./run-this-to-install.sh: Runs make install"
+			exit
+		esac
+
 	touch iteration_dwm_install
 
 	echo "Run this script to install DWM. Just running "make install" will not install all of the components properly, so beware."
 	
-	if [ ! -f "~/.autostart" ]; then 
+	if [ ! -f "$HOME/.autostart" ]; then 
 		cp -v .autostart ~
 	fi
 
@@ -20,7 +28,14 @@ if [ "$EUID" != 0 ]; then
 	exit $?
 
 elif [ "$EUID" = 0 ] && [ -f "iteration_dwm_install" ]; then
-	make install
+	
+	case $1 in
+	"clean")
+		make clean install;;
+	*)
+		make install;;
+	esac
+	
 	rm iteration_dwm_install
 
 else
